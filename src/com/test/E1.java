7c0faae0cfc429dda415bd9975e7a458a69d6435
“此处填写用户名” <1612974150@qq.com>
2021-01-14 18:39:09 +0800

    test

---
+++ src/com/test/E1.java
@@ -0,0 +1,10 @@
+package com.test;
+
+public class E1 {
+    public static void main(String[] args) {
+        int i=1;
+        int j=2;
+        int z =i+j;
+        System.out.println(z);
+    }
+}

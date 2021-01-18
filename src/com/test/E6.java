f89fc37f825ef179e334ea692b1f186b433e9aa6
Draven <65707375+Draven3@users.noreply.github.com>
2021-01-18 22:15:51 +0800

    Update E6.java

--- src/com/test/E6.java
+++ src/com/test/E6.java
@@ -1,20 +1,13 @@
 package com.test;
 
 public class E6 {
-    /*
-    请编写一个命令行程序，定义两个合适的数字类型，分别用于存储以下两个数据：
-    世界人口：71亿
-    中国人口：14亿
-    在命令行输出
-    2019年世界人口总数约：7100000000
-    2019年中国人口总数约：1400000000
- */
+    
 
     public static void main(String[] args) {
-        long worldPopulation = (long) (71 * Math.pow(10, 8));
-        long chinesePopulation = (long) (14 * Math.pow(10, 8));
-        System.out.println("2019年世界人口总数约：" + worldPopulation);
-        System.out.println("2019年中国人口总数约：" + chinesePopulation);
+        long wP = (long) (71 * Math.pow(10, 8));
+        long cP = (long) (14 * Math.pow(10, 8));
+        System.out.println("2019年世界人口总数约：" + wP);
+        System.out.println("2019年中国人口总数约：" + cP);
     }
 }
 

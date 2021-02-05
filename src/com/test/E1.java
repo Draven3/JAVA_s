7c6764dae4b68965f95e6a5fbd104d8b6a2bb034
“此处填写用户名” <1612974150@qq.com>
2021-02-05 23:31:01 +0800

    命令行输入

--- src/com/test/E1.java
+++ src/com/test/E1.java
@@ -2,9 +2,10 @@ package com.test;
 
 public class E1 {
     public static void main(String[] args) {
-        int i=1;
-        int j=2;
-        int z =i+j;
-        System.out.println("i+j="+z);
+/*
+    请编写一个命令行程序，程序运行后在命令行输出以下内容：
+    “虽说长路漫漫，但只要方向正确并坚持下来的人都不会太差！”
+ */
+        System.out.println("虽说长路漫漫，但只要方向正确并坚持下来的人都不会太差！");
     }
 }

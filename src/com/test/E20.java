30c8972c8b72f0942bc8767609c1d8ff018a9341
“此处填写用户名” <1612974150@qq.com>
2021-03-05 17:16:40 +0800

    输出文件夹中文件名

--- src/com/test/E20.java
+++ src/com/test/E20.java
@@ -1,6 +1,7 @@
 package com.test;
 import java.io.File;
 import java.util.Scanner;
+
 /*
     编写一个命令行程序，从命令行输入一个本机地址（也就是本机一个目录的地址），之后程序根据该地址，遍历目录中的所有文件，并将文件名输出到命令行。
     示例：
@@ -11,19 +12,31 @@ import java.util.Scanner;
     xxxxxx.pdf
  */
 public class E20 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
-        File file = new File(str);
-        File[] fileArray = file.listFiles();
-        if (fileArray != null) {
+
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            String str = scanner.next();
+            String rootPath = str;
             System.out.println("目录【"+str+"】中有以下文件");
-            for (File file1 : fileArray) {
+            File file = new File(rootPath);
+            print(0, file);
 
+        }
 
-                System.out.println(file1.getAbsolutePath());
+        public static void print(int n, File file) {
 
+            for (int i = 0; i < n; i++) {
+                System.out.print("　　");
+            }
+
+            System.out.println(file.getName());
+
+            if (file.isDirectory()) {
+                File[] files = file.listFiles();
+                for (File subFile : files) {
+                    print(n + 1, subFile);
+                }
             }
         }
     }
-}
+
